b7dc6cb1e32614c7964b9f41931dd57a3c21c18f
CanftIn <wwc7033@gmail.com>
2018-02-05 19:49:02 +0800

    update

--- renderer/renderer/test.cpp
+++ renderer/renderer/test.cpp
@@ -10,7 +10,6 @@ const TGAColor green = TGAColor(0, 255, 0, 255);
 Model *model = NULL;
 const int width = 800;
 const int height = 800;
-
 void line(Vec2i p0, Vec2i p1, Image &image, TGAColor color) {
 	bool steep = false;
 	if (std::abs(p0.x - p1.x)<std::abs(p0.y - p1.y)) {
@@ -54,8 +53,7 @@ void triangle(Vec2i t0, Vec2i t1, Vec2i t2, Image &image, TGAColor color) {
 	}
 }
 
-int main(int argc, char** argv) 
-{
+int main(int argc, char** argv) {
 	if (2 == argc) {
 		model = new Model(argv[1]);
 	}
@@ -64,18 +62,26 @@ int main(int argc, char** argv)
 	}
 
 	Image image(width, height, Image::RGB);
+	Vec3f light_dir(0, 0, -1);
 	for (int i = 0; i<model->nfaces(); i++) {
 		std::vector<int> face = model->face(i);
 		Vec2i screen_coords[3];
+		Vec3f world_coords[3];
 		for (int j = 0; j<3; j++) {
-			Vec3f world_coords = model->vert(face[j]);
-			screen_coords[j] = Vec2i((world_coords.x + 1.)*width / 2., (world_coords.y + 1.)*height / 2.);
+			Vec3f v = model->vert(face[j]);
+			screen_coords[j] = Vec2i((v.x + 1.)*width / 2., (v.y + 1.)*height / 2.);
+			world_coords[j] = v;
 		}
-		triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(rand() % 255, rand() % 255, rand() % 255, 255));
+		Vec3f n = (world_coords[2] - world_coords[0]) ^ (world_coords[1] - world_coords[0]);
+		n.normalize();
+		float intensity = n*light_dir;
+		if (intensity>0) {
+			triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
+		}
 	}
 
-	image.flip_vertically();
-	image.write_tga_file("output2.tga");
+	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+	image.write_tga_file("output.tga");
 	delete model;
 	return 0;
 }
